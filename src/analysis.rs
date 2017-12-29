b73593b6d2298ec03b57466ada071e38d1009c49
David Hacker <dmhacker@yahoo.com>
2017-12-29 04:51:56 +0000

    rbtables assumes multiple seeds will be added upon initialization

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,28 +1,35 @@
 
 use std::collections::HashMap;
 
-pub struct Table<'a> {
+pub struct RainbowTable<'a> {
   chains: HashMap<&'a str, &'a str>,
   hashing_function: fn(&str) -> u8,
   reduction_functions: Vec<fn(u8) -> &'a str>
 }
 
-impl<'a> Table<'a> {
+impl<'a> RainbowTable<'a> {
 
-  fn new(hashing_function : fn(&str) -> u8, reduction_functions : Vec<fn(u8) -> &'a str>) -> Table<'a> {
-    Table {
+  fn new(hashing_function : fn(&str) -> u8, reduction_functions : Vec<fn(u8) -> &'a str>) -> RainbowTable<'a> {
+    RainbowTable {
       chains: HashMap::new(),
       hashing_function: hashing_function,
       reduction_functions: reduction_functions
     }
   }
 
-  fn add_seed(&'a mut self, seed : &'a str) -> &'a mut Table {
-    if !self.chains.contains_key(seed) {
-      self.chains.insert(seed, seed);
-      for reduction_function in &self.reduction_functions {
-        let next_value = (reduction_function)((self.hashing_function)(self.chains.get(seed).unwrap()));
-        self.chains.insert(seed, &next_value);
+  fn add_seed(&'a mut self, seed : &'a str) -> &'a mut RainbowTable {
+    let wrapper = vec![seed];
+    self.add_seeds(&wrapper)
+  }
+
+  fn add_seeds(&'a mut self, seeds : &[&'a str]) -> &'a mut RainbowTable {
+    for seed in seeds {
+      if !self.chains.contains_key(seed) {
+        self.chains.insert(seed, seed);
+        for reduction_function in &self.reduction_functions {
+          let next_value = (reduction_function)((self.hashing_function)(self.chains.get(seed).unwrap()));
+          self.chains.insert(seed, &next_value);
+        }
       }
     }
     self
