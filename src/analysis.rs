f2683c7fb3040efdb91e852a160f4d5b70a537f0
David Hacker <dmhacker@yahoo.com>
2018-01-02 21:02:17 +0000

    Remove lifetime, get started on multithreading

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,15 +1,17 @@
 
 use std::collections::HashMap;
+use std::sync::{Arc, RwLock, mpsc};
+use std::thread;
 
-pub struct RainbowTable<'a> {
+pub struct RainbowTable {
   chains: HashMap<String, String>,
-  hashing_function: fn(&str) -> &'a [u8],
-  reduction_functions: Vec<fn(&[u8]) -> &'a str>
+  hashing_function: fn(&str) -> &[u8],
+  reduction_functions: Vec<fn(&[u8]) -> &str>
 }
 
-impl<'a> RainbowTable<'a> {
+impl RainbowTable {
 
-  fn new(hashing_function : fn(&str) -> &'a [u8], reduction_functions : Vec<fn(&[u8]) -> &'a str>) -> RainbowTable<'a> {
+  fn new(hashing_function : fn(&str) -> &[u8], reduction_functions : Vec<fn(&[u8]) -> &str>) -> RainbowTable {
     RainbowTable {
       chains: HashMap::new(),
       hashing_function: hashing_function,
@@ -17,12 +19,12 @@ impl<'a> RainbowTable<'a> {
     }
   }
 
-  fn add_seed(&'a mut self, seed : &'a str) -> &'a mut RainbowTable {
+  fn add_seed(&mut self, seed : &str) -> &mut RainbowTable {
     let wrapper = vec![seed];
     self.add_seeds(&wrapper)
   }
 
-  fn add_seeds(&'a mut self, seeds : &[&'a str]) -> &'a mut RainbowTable {
+  fn add_seeds(&mut self, seeds : &[&str]) -> &mut RainbowTable {
     for seed in seeds {
       let mut next_value = seed as &str;
       for reduction_function in &self.reduction_functions {
@@ -33,7 +35,7 @@ impl<'a> RainbowTable<'a> {
     self
   }
 
-  fn find_plaintext(&'a self, hash : &[u8]) -> Option<&'a str> {
+  fn find_plaintext(&self, hash : &[u8]) -> Option<&str> {
     let rf_len = self.reduction_functions.len();
     let mut current_plaintext = "";
     let mut current_hash = hash;
@@ -55,6 +57,40 @@ impl<'a> RainbowTable<'a> {
       }
       current_hash = hash;
     }
+    None
+  }
+
+  fn find_plaintext_threaded(&self, hash : &[u8], num_threads : u8) -> Option<&str>  {
+    assert!(num_threads > 1);
+
+    let rfs = &self.reduction_functions;
+    let hf = &self.hashing_function;
+
+    let rf_len : usize = self.reduction_functions.len();
+    let rf_playbacks : Vec<usize> = (0..rf_len).collect();
+    let counter = Arc::new(RwLock::new(0));
+    // let (tx, rx) = mpsc::channel();
+
+    for _ in 0..num_threads {
+      let counter = Arc::clone(&counter);
+      let _ = thread::spawn(move || {
+        let current_playback = counter.read().unwrap();
+        while *current_playback < rf_len {
+          /*
+          let mut current_plaintext = "";
+          let mut current_hash = hash;
+          for j in *current_playback..rf_len {
+            current_plaintext = (rfs[j])(current_hash);
+            current_hash = (hf)(current_plaintext);
+          }
+          */
+
+          let mut adjust_playback = counter.write().unwrap();
+          *adjust_playback += 1;
+        }
+      }).join();
+    }
+
     None
   }
 }
